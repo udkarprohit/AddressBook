939765f8aa883e510eaf2bb8eef4654131b263fa
Prohit <udkarprohit@gmail.com>
2023-01-19 11:21:07 +0530

    Added: New contacts of an user

--- src/com/bridgelabz/AddressBook.java
+++ src/com/bridgelabz/AddressBook.java
@@ -4,7 +4,35 @@ import java.util.Scanner;
 
 public class AddressBook {
 
+    //creating an instance variable of AddressBook class
+    Contacts contacts = new Contacts();
+
     void addContacts() {
-        System.out.println("addContacts()");
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Enter First Name : ");
+        String firstName = scanner.nextLine();
+        System.out.print("Enter Last Name : ");
+        String lastName = scanner.nextLine();
+        System.out.print("Enter Address : ");
+        String address = scanner.nextLine();
+        System.out.print("Enter City Name : ");
+        String cityName = scanner.nextLine();
+        System.out.print("Enter State Name : ");
+        String stateName = scanner.nextLine();
+        System.out.print("Enter Zip Code : ");
+        String zipCode = scanner.nextLine();
+        System.out.print("Enter Phone Number : ");
+        String phoneNumber = scanner.nextLine();
+        System.out.print("Enter Email : ");
+        String email = scanner.nextLine();
+        //setting attributes values form users end.
+        contacts.setFirstName(firstName);
+        contacts.setLastName(lastName);
+        contacts.setAddress(address);
+        contacts.setCityName(cityName);
+        contacts.setStateName(stateName);
+        contacts.setZipCode(zipCode);
+        contacts.setPhoneNumber(phoneNumber);
+        contacts.setEmail(email);
     }
 }
--- src/com/bridgelabz/AddressBookMain.java
+++ src/com/bridgelabz/AddressBookMain.java
@@ -6,6 +6,19 @@ public class AddressBookMain {
         AddressBook addressBook = new AddressBook();
         System.out.println("Welcome to Address BookProgram");
 
+        /*calling non-static method
+        or object-level method */
         addressBook.addContacts();
+
+        System.out.println();
+        //getting the values of users(get method)
+        System.out.println("First Name : " + addressBook.contacts.getFirstName());
+        System.out.println("Last Name : " + addressBook.contacts.getLastName());
+        System.out.println("Address : " + addressBook.contacts.getAddress());
+        System.out.println("City Name : " + addressBook.contacts.getCityName());
+        System.out.println("State Name : " + addressBook.contacts.getStateName());
+        System.out.println("Zip Code : " + addressBook.contacts.getZipCode());
+        System.out.println("Phone Number : " + addressBook.contacts.getPhoneNumber());
+        System.out.println("Email : " + addressBook.contacts.getEmail());
     }
 }
